70a6337bce2d733d23240b2417afc642cad5c107
michalhagos <mhayele3@gmail.com>
2026-05-20 15:22:40 -0500

    created a person class and added private fields firstName lastName and age

--- src/main/java/com/pluralsight/loops/Person.java
+++ src/main/java/com/pluralsight/loops/Person.java
@@ -1,4 +1,12 @@
-package com.pluralsight;
+package com.pluralsight.loops;
 
 public class Person {
+    // the first name of the person
+    private String firstName;
+    // the last name of the person
+    private String lastName;
+    // the age of the person
+    private int age;
+
+
 }
---
+++ src/main/java/com/pluralsight/loops/Program.java
@@ -0,0 +1,7 @@
+package com.pluralsight.loops;
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
